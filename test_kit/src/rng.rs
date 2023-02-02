6f14b6a63e4693d5105d36fc5551bbe6793f517a
Vin Singh <theshampoofactory@gmail.com>
2023-02-02 19:29:51 +0000

    Rng add check_bytes method

--- test_kit/src/rng.rs
+++ test_kit/src/rng.rs
@@ -1,3 +1,4 @@
+use std::convert::TryInto;
 use std::mem;
 
 /// Deterministic random number generator.
@@ -13,6 +14,28 @@ impl Rng {
     pub fn gen(&mut self) -> u32 {
         self.0 = self.0.wrapping_mul(1_103_515_245).wrapping_add(12_345);
         self.0
+    }
+
+    pub fn check_bytes(&mut self, bytes: &[u8]) -> bool {
+        let mut index = 0;
+        while index <= bytes.len() - 4 {
+            let src: [u8; 4] = (bytes[index..index + 4]).try_into().unwrap();
+            let u = u32::from_ne_bytes(src);
+            if u != self.0 {
+                return false;
+            }
+            self.gen();
+            index += 4;
+        }
+        let mut u = self.0;
+        while index < bytes.len() {
+            if bytes[index] != u as u8 {
+                return false;
+            }
+            u >>= 8;
+            index += 1;
+        }
+        true
     }
 
     pub fn gen_vec(&mut self, len: usize) -> Option<Vec<u8>> {
@@ -47,5 +70,18 @@ impl Iterator for Rng {
     #[inline(always)]
     fn next(&mut self) -> Option<Self::Item> {
         Some(self.gen())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_bytes() {
+        let mut rng = Rng::default();
+        let bytes = rng.gen_vec(1023).unwrap();
+        let mut rng = Rng::default();
+        assert!(rng.check_bytes(&bytes));
     }
 }
