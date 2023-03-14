3f02af87530458fa6547fd6e10444a81e1c2fdf0
Vin Singh <theshampoofactory@gmail.com>
2023-03-14 19:06:15 +0000

    Add FrontendBytes big mem tests

--- test/src/big_mem.rs
+++ test/src/big_mem.rs
@@ -1,5 +1,5 @@
 // FrontendBytes big memory tests.
-macro_rules! test_pattern {
+macro_rules! test_pattern_rng {
     ($name:ident, $encoder:expr, $len:expr) => {
         mod $name {
             use lzfse_rust::{LzfseDecoder, LzfseEncoder};
@@ -28,7 +28,75 @@ macro_rules! test_pattern {
     };
 }
 
-test_pattern!(big_2gb_sub_1, ops::encode, 0x7FFF_FFFF);
-test_pattern!(big_2gb, ops::encode, 0x8000_0000);
 #[cfg(target_pointer_width = "64")]
-test_pattern!(big_8gb, ops::encode, 0x2_0000_0000);
+test_pattern_rng!(big_rng_512mb, ops::encode, 0x2000_0000);
+#[cfg(target_pointer_width = "64")]
+test_pattern_rng!(big_rng_7FFF_FFFD, ops::encode, 0x7FFF_FFFD);
+#[cfg(target_pointer_width = "64")]
+test_pattern_rng!(big_rng_7FFF_FFFE, ops::encode, 0x7FFF_FFFE);
+#[cfg(target_pointer_width = "64")]
+test_pattern_rng!(big_rng_7FFF_FFFF, ops::encode, 0x7FFF_FFFF);
+#[cfg(target_pointer_width = "64")]
+test_pattern_rng!(big_rng_8000_0000, ops::encode, 0x8000_0000);
+#[cfg(target_pointer_width = "64")]
+test_pattern_rng!(big_rng_8000_0001, ops::encode, 0x8000_0001);
+#[cfg(target_pointer_width = "64")]
+test_pattern_rng!(big_rng_8000_0002, ops::encode, 0x8000_0002);
+#[cfg(target_pointer_width = "64")]
+test_pattern_rng!(big_rng_8000_0003, ops::encode, 0x8000_0003);
+#[cfg(target_pointer_width = "64")]
+test_pattern_rng!(big_rng_8000_0004, ops::encode, 0x8000_0004);
+#[cfg(target_pointer_width = "64")]
+test_pattern_rng!(big_rng_2_0000_0000, ops::encode, 0x2_0000_0000);
+
+macro_rules! test_pattern_zeros {
+    ($name:ident, $encoder:expr, $len:expr) => {
+        mod $name {
+            use lzfse_rust::{LzfseDecoder, LzfseEncoder};
+
+            use std::io;
+
+            #[test]
+            fn zeros() -> io::Result<()> {
+                let mut dec = Vec::default();
+                {
+                    let mut enc = Vec::with_capacity($len / 4);
+                    {
+                        let src = vec![0; $len];
+                        LzfseEncoder::default().encode_bytes(&src, &mut enc)?;
+                        // src drops, free src memory
+                    }
+                    LzfseDecoder::default().decode_bytes(&enc, &mut dec)?;
+                    // enc drops, free enc memory
+                }
+                assert_eq!(dec.len(), $len);
+                for b in dec {
+                    assert_eq!(b, 0);
+                }
+                Ok(())
+            }
+        }
+    };
+}
+
+// TODO should be consistent with rng
+#[cfg(target_pointer_width = "64")]
+test_pattern_zeros!(big_zeros_512mb, ops::encode, 0x2000_0000);
+#[cfg(target_pointer_width = "64")]
+test_pattern_zeros!(big_zeros_7FFF_FFFD, ops::encode, 0x7FFF_FFFD);
+#[cfg(target_pointer_width = "64")]
+test_pattern_zeros!(big_zeros_7FFF_FFFE, ops::encode, 0x7FFF_FFFE);
+#[cfg(target_pointer_width = "64")]
+test_pattern_zeros!(big_zeros_7FFF_FFFF, ops::encode, 0x7FFF_FFFF);
+#[cfg(target_pointer_width = "64")]
+test_pattern_zeros!(big_zeros_8000_0000, ops::encode, 0x8000_0000);
+#[cfg(target_pointer_width = "64")]
+test_pattern_zeros!(big_zeros_8000_0001, ops::encode, 0x8000_0001);
+#[cfg(target_pointer_width = "64")]
+test_pattern_zeros!(big_zeros_8000_0002, ops::encode, 0x8000_0002);
+#[cfg(target_pointer_width = "64")]
+test_pattern_zeros!(big_zeros_8000_0003, ops::encode, 0x8000_0003);
+#[cfg(target_pointer_width = "64")]
+test_pattern_zeros!(big_zeros_8000_0004, ops::encode, 0x8000_0004);
+#[cfg(target_pointer_width = "64")]
+test_pattern_zeros!(big_zeros_2_0000_0000, ops::encode, 0x2_0000_0000);
